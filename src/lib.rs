137ccd2bde1019dc9b5000ae2542b1a64697b6f2
jawang94 <wang.jason.010@gmail.com>
2024-06-02 15:56:29 -0700

    Why did these not get staged??

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,6 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscribe", web::get().to(subscribe))
-            .route("/{name}", web::get().to(greet))
-            .route("/", web::get().to(greet))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
-
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
@@ -25,8 +11,27 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-async fn subscribe() -> impl Responder {
-    let todo_panics = std::panic::catch_unwind(|| todo!("/subscribe route incomplete")).is_err();
-    assert!(todo_panics);
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    // let todo_panics = std::panic::catch_unwind(|| todo!("/subscribe route incomplete")).is_err();
+    // assert!(todo_panics);
     HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/subscriptions", web::post().to(subscribe))
+            .route("/health_check", web::get().to(health_check))
+            .route("/{name}", web::get().to(greet))
+            .route("/", web::get().to(greet))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
 }
