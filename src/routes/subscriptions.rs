e0c88ef513340cb760a343abc536535c9a560683
jawang94 <wang.jason.010@gmail.com>
2024-07-21 20:52:27 -0700

    Impl DatabaseSettings connection_strng && write unit test for 200 post to our DB on subscribe (partial)

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,12 +1,12 @@
 use actix_web::{web, HttpResponse};
 
-#[derive(serde::Deserialize)]
-pub struct FormData {
-    name: String,
-    email: String,
+#[derive(serde::Deserialize, Debug)]
+pub struct SubscriptionData {
+    pub name: String,
+    pub email: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(_form: web::Form<SubscriptionData>) -> HttpResponse {
     // let todo_panics = std::panic::catch_unwind(|| todo!("/subscribe route incomplete")).is_err();
     // assert!(todo_panics);
     HttpResponse::Ok().finish()
--- tests/health_check_test.rs
+++ tests/health_check_test.rs
@@ -1,4 +1,8 @@
+use common::spawn_app;
 use reqwest;
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
+use zero2prod::routes::SubscriptionData;
 mod common;
 
 #[tokio::test]
@@ -13,4 +17,38 @@ async fn health_check_works() {
         .expect("Failed to execute request.");
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    // The `Connection` trait MUST be in scope for us to invoke `PgConnection::connect`
+    // It is not an inherent method of the struct...kinda crazy
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+    let client = reqwest::Client::new();
+    // Act
+    let body = "name=jason%20wang&email=jason_wang@gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+
+    // Below will error since our /subscriptions route is not implemented yet
+    // let saved = sqlx::query_as!(SubscriptionData, "SELECT email, name FROM subscriptions",)
+    //     .fetch_one(&mut connection)
+    //     .await
+    //     .expect("Failed to fetch saved subscription.");
+
+    // assert_eq!(saved.email, "jason_wang@gmail.com");
+    // assert_eq!(saved.name, "jason wang");
 }
